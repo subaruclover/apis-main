260d998811e6739818827a6a04f7574134dbe5ba
Qiong <subaru6725@hotmail.com>
2021-10-08 15:23:12 +0900

    generate scenario.json file

--- exe/createScenario.py
+++ exe/createScenario.py
@@ -1,5 +1,7 @@
 import json
 
+timePeriods = ["00:00:00-12:00:00", "12:00:00-24:00:00"]
+
 data = {
     "refreshingPeriodMsec": 5000,
 
@@ -7,58 +9,83 @@ data = {
         "strategy": "pointAndAmount"
     },
 
-    "00:00:00-24:00:00": {
+    timePeriods[0]: {
         "batteryStatus": {
-            "4320-": "excess",
-            "3600-4320": "sufficient",
-            "2880-3600": "scarce",
-            "-2880": "short"
+            "3840-": "excess",
+            "2400-3840": "sufficient",
+            "1920-2400": "scarce",
+            "-1920": "short"
         },
         "request": {
-            "excess": {
-                "discharge": {
-                    "limitWh": 4320,
-                    "pointPerWh": 10
-                }
-            },
-            "sufficient": {
-            },
-            "scarce": {
-            },
-            "short": {
-                "charge": {
-                    "limitWh": 2880,
-                    "pointPerWh": 10
-                }
-            }
+            "excess": {"discharge": {
+                "limitWh": 3840,
+                "pointPerWh": 10
+            }},
+            "sufficient": {},
+            "scarce": {},
+            "short": {"charge": {
+                "limitWh": 1920,
+                "pointPerWh": 10
+            }}
         },
         "accept": {
-            "excess": {
-                "discharge": {
-                    "limitWh": 3600,
-                    "pointPerWh": 10
-                }
-            },
-            "sufficient": {
-                "discharge": {
-                    "limitWh": 3600,
-                    "pointPerWh": 10
-                }
-            },
-            "scarce": {
-                "charge": {
-                    "limitWh": 3600,
-                    "pointPerWh": 10
-                }
-            },
-            "short": {
-                "charge": {
-                    "limitWh": 3600,
-                    "pointPerWh": 10
-                }
-            }
+            "excess": {"discharge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "sufficient": {"discharge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "scarce": {"charge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "short": {"charge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }}
         }
     },
+
+    timePeriods[1]: {
+        "batteryStatus": {
+            "3360-": "excess",
+            "2400-3360": "sufficient",
+            "1440-2400": "scarce",
+            "-1440": "short"
+        },
+        "request": {
+            "excess": {"discharge": {
+                "limitWh": 3360,
+                "pointPerWh": 10
+            }},
+            "sufficient": {},
+            "scarce": {},
+            "short": {"charge": {
+                "limitWh": 1440,
+                "pointPerWh": 10
+            }}
+        },
+        "accept": {
+            "excess": {"discharge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "sufficient": {"discharge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "scarce": {"charge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }},
+            "short": {"charge": {
+                "limitWh": 2400,
+                "pointPerWh": 10
+            }}
+        }
+    }
 }
 
 # generate scenario.json file
