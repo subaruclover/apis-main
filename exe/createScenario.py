69fdbc69f0074c5e97aaf09ff89831b3535fd910
Qiong <subaru6725@hotmail.com>
2021-10-13 23:43:32 +0900

    refreshing scenario file using time

--- exe/createScenario.py
+++ exe/createScenario.py
@@ -1,11 +1,15 @@
+# create by Qiong
+# create scenario files for updating the energy exchange rules
+
 import json
+import time
 
 # set time periods for scenario files
 timePeriods = ["00:00:00-12:00:00", "12:00:00-24:00:00"]
 # battery Size: 4800
 batterySize = 4800
 
-data = {
+data1 = {
     "refreshingPeriodMsec": 5000,
 
     "acceptSelection": {
@@ -14,10 +18,10 @@ data = {
 
     timePeriods[0]: {
         "batteryStatus": {
-            str(batterySize*0.8): "excess",
+            str(batterySize*0.8) + "-": "excess",
             str(str(batterySize*0.5) + "-" + str(batterySize*0.8)): "sufficient",
             str(str(batterySize*0.4) + "-" + str(batterySize*0.5)): "scarce",
-            str(batterySize*0.4): "short"
+            "-" + str(batterySize*0.4): "short"
         },
         "request": {
             "excess": {"discharge": {
@@ -53,38 +57,38 @@ data = {
 
     timePeriods[1]: {
         "batteryStatus": {
-            "3360-": "excess",
-            "2400-3360": "sufficient",
-            "1440-2400": "scarce",
-            "-1440": "short"
+            str(batterySize*0.7) + "-": "excess",
+            str(str(batterySize*0.5) + "-" + str(batterySize*0.7)): "sufficient",
+            str(str(batterySize*0.3) + "-" + str(batterySize*0.5)): "scarce",
+            "-" + str(batterySize*0.3): "short"
         },
         "request": {
             "excess": {"discharge": {
-                "limitWh": 3360,
+                "limitWh": batterySize*0.7,
                 "pointPerWh": 10
             }},
             "sufficient": {},
             "scarce": {},
             "short": {"charge": {
-                "limitWh": 1440,
+                "limitWh": batterySize*0.3,
                 "pointPerWh": 10
             }}
         },
         "accept": {
             "excess": {"discharge": {
-                "limitWh": 2400,
+                "limitWh": batterySize*0.5,
                 "pointPerWh": 10
             }},
             "sufficient": {"discharge": {
-                "limitWh": 2400,
+                "limitWh": batterySize*0.5,
                 "pointPerWh": 10
             }},
             "scarce": {"charge": {
-                "limitWh": 2400,
+                "limitWh": batterySize*0.5,
                 "pointPerWh": 10
             }},
             "short": {"charge": {
-                "limitWh": 2400,
+                "limitWh": batterySize*0.5,
                 "pointPerWh": 10
             }}
         }
@@ -92,5 +96,16 @@ data = {
 }
 
 # generate scenario.json file
-with open('scenario.json', 'w') as jsonfile:
-    json.dump(data, jsonfile)
+t_end = time.time() + 60 * 15  # run for 15 min x 60 s = 900 seconds.
+while time.time() < t_end:
+    print(time.time())
+
+    with open('scenario.json', 'w') as jsonfile:
+        json.dump(data1, jsonfile)
+
+    print("scenario file updated")
+    # refresh every 5 seconds
+    time.sleep(5)
+
+# with open('scenario.json', 'w') as jsonfile:
+#     json.dump(data1, jsonfile)
